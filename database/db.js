a6a347dc7677e35c11d7ac77506225183930a5e0
Egeeoz <ozakdemirege@hotmail.com>
2024-05-15 09:44:38 +0200

    Poop

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
-const tableDefinitions = require('./definitions/tableDefinitions');
-const viewDefinitions = require('./definitions/viewDefinitions');
+const tableDefinitions = require("./definitions/tableDefinitions");
+const viewDefinitions = require("./definitions/viewDefinitions");
 
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
 const initDatabase = () => {
   const db = new sqlite3.Database("./database/database.db", (error) => {
@@ -33,9 +33,51 @@ const initDatabase = () => {
       .run(usersChannelsTable, (error) => {
         if (error) console.error(error);
       });
+  };
+
+  const createViews = () => {
+    db.run(subscribersView)
+      .run(messagesFromUser)
+      .run(messagesForChannel)
+      .run(channelOwnedByUser);
+  };
+
+  const createRelationMessagesChannelsTable = (messageId, channelId) => {
+    return new Promise((resolve, reject) => {
+      db.run(
+        "INSERT INTO messagesChannels (Message_Id, Channel_Id) VALUES (?, ?)",
+        [messageId, channelId],
+        (error) => {
+          if (error) reject(error);
+          resolve();
+        }
+      );
+    });
+  };
+
+  const createRelationUsersChannelsTable = (userId, channelId) => {
+    return new Promise((resolve, reject) => {
+      db.run(
+        "INSERT INTO usersChannels (User_Id, Channel_Id) VALUES (?, ?)",
+        [userId, channelId],
+        (error) => {
+          if (error) reject(error);
+          resolve();
+        }
+      );
+    });
+  };
+
+  db.serialize(() => {
+    createTables();
+    createViews();
   });
 
-  return db;
+  return {
+    db,
+    createRelationUsersChannelsTable,
+    createRelationMessagesChannelsTable,
+  };
 };
 
 module.exports = initDatabase;
