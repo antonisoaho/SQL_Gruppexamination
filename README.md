6093654048058ce0ec3911bd71500f1b3697a260
antonisoaho <isoahoanton@gmail.com>
2024-05-19 21:01:49 +0200

    Changing to primarykeys in many-many

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # SQL_Gruppexamination
-https://lucid.app/lucidchart/adc35c82-1bfb-4f60-bb5b-b3c51c4998ad/edit?invitationId=inv_cf1097a7-7713-4c58-b785-bd6499eefd39
 
 Ege Özakdemir, Ahmed Abbas, Martin Kolberg, Anton Isoaho
+
+![alt text](image.png)
--- database/definitions/tableDefinitions.js
+++ database/definitions/tableDefinitions.js
@@ -8,11 +8,21 @@ const tableDefinitions = {
   messagesTable:
     'CREATE TABLE IF NOT EXISTS messages(Id INTEGER PRIMARY KEY, Message TEXT, Created_At DATETIME, User_Id INTEGER, FOREIGN KEY (User_Id) REFERENCES users(Id));',
 
-  messagesChannelsTable:
-    'CREATE TABLE IF NOT EXISTS messagesChannels(Message_Id INTEGER, Channel_Id INTEGER, FOREIGN KEY (Message_Id) REFERENCES messages(Id), FOREIGN KEY (Channel_Id) REFERENCES channels(Id))',
+  messagesChannelsTable: `CREATE TABLE IF NOT EXISTS messagesChannels (
+    Message_Id INTEGER,
+    Channel_Id INTEGER,
+    PRIMARY KEY (Message_Id, Channel_Id),
+    FOREIGN KEY (Message_Id) REFERENCES messages(Id),
+    FOREIGN KEY (Channel_Id) REFERENCES channels(Id)
+  );`,
 
-  usersChannelsTable:
-    'CREATE TABLE IF NOT EXISTS usersChannels(User_Id INTEGER, Channel_Id INTEGER, FOREIGN KEY (User_Id) REFERENCES users(Id), FOREIGN KEY (Channel_Id) REFERENCES channels(Id))',
+  usersChannelsTable: `CREATE TABLE IF NOT EXISTS usersChannels (
+    User_Id INTEGER,
+    Channel_Id INTEGER,
+    PRIMARY KEY (User_Id, Channel_Id),
+    FOREIGN KEY (User_Id) REFERENCES users(Id),
+    FOREIGN KEY (Channel_Id) REFERENCES channels(Id)
+  );`,
 };
 
 module.exports = tableDefinitions;
